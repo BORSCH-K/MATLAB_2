cf7f48f85738381063691917ec54f4d92228d201
Ксения <urukinaksusha@gmail.com>
2023-03-22 23:38:54 +0600

    final lab_1

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,43 +1,120 @@
 import numpy as np
 import scipy as sp
+from scipy import signal as sg
 import matplotlib.pyplot as plt
 
+
 def main():
-    fd = 1024  # частота дискретизации
-    f = 16  # частота сигнала
+    fd = 1024  # частота дискретизации; сколько раз за секунду измеряется моментальное значение сигнала
+    f = 16  # частота сигнала; длина шага
     ph = 0  # начальная фаза
-    k = fd / f  # шаг
+    N = fd / f  # число точек на периоде
     A = 1  # амплитуда
-    t = np.arange(0, k)
-    y = A * np.sin(2 * np.pi * t / k + ph)
+    n = np.arange(0, N)  # номер шага
+    y = A * np.sin(2 * np.pi * n / N + ph)
 
     fig, ax = plt.subplots()
-    fig1, ax1 = plt.subplots()
-    ax.plot(t / fd, y)
+    ax.plot(n / fd, y)  # период сигнала = 0,0625
     ax.grid()
     ax.set_xlabel('t,c')
     ax.set_ylabel('A')
 
-    CS = np.fft.fft(y)
-    AS = np.abs(CS)
-    FS = np.angle(CS)
-    ax1.set_xlabel('Сдвиг фазы')
-    ax1.set_ylabel('w,Гц')
-    ax1.plot(t * f, FS)
-    ax1.grid()
-    fig3, ax3 = plt.subplots()
-    ax3.stem(t * f, AS)
-    ax3.set_xlabel('Амплитуда')
-    ax3.set_ylabel('w , Гц')
-    ax3.grid()
-    z = np.fft.ifft(CS)
-    fig4, ax4 = plt.subplots()
-    ax4.plot(t / fd, z)
-    ax4.set_xlabel('t , c')
-    ax4.set_ylabel('A')
-    ax4.grid()
-    plt.show()
+    # Скважность
+    fig2, ax2 = plt.subplots()
+    y1 = A * sg.square(2 * np.pi * n / N + ph, duty=0.5)
+    ax2.plot(n / fd, y1)
+    ax2.grid()
+    ax2.set_xlabel('t,c')
+    ax2.set_ylabel('A')
 
+    fig3, ax3 = plt.subplots()
+    y2 = A * sg.square(2 * np.pi * n / N + ph, duty=0.25)
+    ax3.plot(n / fd, y2)
+    ax3.grid()
+    ax3.set_xlabel('t,c')
+    ax3.set_ylabel('A')
+
+    # ПД, АЧХ, ФЧХ
+
+    CS = np.fft.fft(y)
+
+    fig4, ax4 = plt.subplots()
+    AS = np.abs(CS)
+    ax4.stem(n * f, AS)
+    ax4.grid()
+    ax4.set_xlabel('w,Гц')
+    ax4.set_ylabel('Aмплитуда')
+
+    fig5, ax5 = plt.subplots()
+    FS = np.angle(CS)
+    ax5.plot(n * f, FS)
+    ax5.grid()
+    ax5.set_xlabel('w,Гц')
+    ax5.set_ylabel('Сдвиг фазы')
+
+    CS1 = np.fft.fft(y1)
+
+    fig6, ax6 = plt.subplots()
+    AS1 = np.abs(CS1)
+    ax6.stem(n * f, AS1)
+    ax6.grid()
+    ax6.set_xlabel('w,Гц')
+    ax6.set_ylabel('Aмплитуда')
+
+    fig7, ax7 = plt.subplots()
+    FS1 = np.angle(CS1)
+    ax7.plot(n * f, FS1)
+    ax7.grid()
+    ax7.set_xlabel('w,Гц')
+    ax7.set_ylabel('Сдвиг фазы')
+
+    CS2 = np.fft.fft(y2)
+
+    fig8, ax8 = plt.subplots()
+    AS2 = np.abs(CS2)
+    ax8.stem(n * f, AS2)
+    ax8.grid()
+    ax8.set_xlabel('w,Гц')
+    ax8.set_ylabel('Aмплитуда')
+
+    fig9, ax9 = plt.subplots()
+    FS2 = np.angle(CS1)
+    ax9.plot(n * f, FS2)
+    ax9.grid()
+    ax9.set_xlabel('w,Гц')
+    ax9.set_ylabel('Сдвиг фазы')
+
+    # востановление графиков
+    z = np.fft.ifft(CS)
+    # z1 = np.ifft(CS1)
+    # z2 = np.ifft(CS2)
+
+    fig10, ax10 = plt.subplots()
+    ax10.plot(n / fd, z)
+    ax10.grid()
+    ax10.set_xlabel('t,c')
+    ax10.set_ylabel('A')
+
+    # 2Т
+
+    n_2 = np.arange(0, 2 * N)
+    y_2 = A * np.sin(2 * np.pi * n_2 / N + ph)
+
+    fig11, ax11 = plt.subplots()
+    ax11.plot(n_2 / fd, y_2)
+    ax11.grid()
+    ax11.set_xlabel('t,c')
+    ax11.set_ylabel('A')
+
+    CS_2 = np.fft.fft(y_2)
+    AS_2 = np.abs(CS_2)
+    fig12, ax12 = plt.subplots()
+    ax12.stem(n_2 * f/2, AS_2)
+    ax12.grid()
+    ax12.set_xlabel('w,Гц')
+    ax12.set_ylabel('Aмплитуда')
+
+    plt.show()
 
 
 if __name__ == '__main__':
